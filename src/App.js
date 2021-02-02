5b48d5832f4dab2e2819aa7df838453a90d3f313
Sergii Sergiienko <sergusha.s@gmail.com>
2021-02-02 14:50:35 +0200

    React+замена src (formmobail)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import FormMobail from "./components/FormMobail";
+
+function App() {
+  return (
+    <>
+    <FormMobail />
+    </>
+       
+  );
+}
+
+export default App;
